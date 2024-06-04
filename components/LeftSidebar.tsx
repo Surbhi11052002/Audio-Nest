385fcfcea5ca132ae37eb63745bdc8cd3850b88a
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2024-06-04 18:02:30 +0530

    hompage layout completed

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
 import { Section } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,14 +11,14 @@ const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
   return (
-    <section className="left-sidebar">
+    <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
         <Link
           href="/"
           className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
         >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
-          <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden">
             Audio Nest
           </h1>
         </Link>
@@ -28,10 +29,15 @@ const LeftSidebar = () => {
             <Link
               href={route}
               key={label}
-              className="flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start"
+              className={cn(
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                }
+              )}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
-              <p className="text-white-1">{label}</p>
+              <p>{label}</p>
             </Link>
           );
         })}
