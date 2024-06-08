f470be89acc79ab123e2d29708d2f8b10db316c3
Surbhi <94106436+Surbhi11052002@users.noreply.github.com>
2024-06-08 20:58:45 +0530

    clerk setup done

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://amused-wahoo-56.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+export default authConfig;
